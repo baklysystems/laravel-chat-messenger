36c31d84b7e56dea1e15c93ae5e303d41221c838
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2017-10-24 16:08:29 +0200

    package files

--- src/LaravelMessengerServiceProvider.php
+++ src/LaravelMessengerServiceProvider.php
@@ -13,7 +13,32 @@ class LaravelMessengerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // config file.
+        $this->publishes([
+            __DIR__.'/config/messenger.php' => config_path('messenger.php'),
+        ]);
+
+        // routes.
+        $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
+
+        // migrations.
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        // publish under messenger-migrations tag.
+        $this->publishes([
+            __DIR__.'/database/migrations' => database_path('migrations'),
+        ], 'messenger-migrations');
+
+        // views.
+        $this->loadViewsFrom(__DIR__.'/views', 'messenger');
+        // publish under messenger-views tag.
+        $this->publishes([
+            __DIR__.'/views' => resource_path('views/vendor/messenger'),
+        ], 'messenger-views');
+
+        // assets.
+        $this->publishes([
+            __DIR__.'/assets' => public_path('vendor/messenger'),
+        ], 'public');
     }
 
     /**

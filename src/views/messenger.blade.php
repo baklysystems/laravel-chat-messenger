2c0fea2d010ce9fb3778569bb4057ef17049b488
Mostafa El Bakly <elbakly@gmail.com>
2017-10-31 16:54:28 +0200

    using font-awesome cdn

--- src/views/messenger.blade.php
+++ src/views/messenger.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css-styles')
-    <link rel="stylesheet" href="/vendor/messenger/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/vendor/messenger/css/messenger.css">
 @endsection
 
